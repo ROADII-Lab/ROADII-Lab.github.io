1c06953ae319040894277945aa9ff1b2187c78de
wchupp <william.chupp@dot.gov>
2023-01-12 10:48:37 -0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-header">
+        TRIMS Home Page
+      </h1>
+    </div>
+  );
+}
+
+export default App;
