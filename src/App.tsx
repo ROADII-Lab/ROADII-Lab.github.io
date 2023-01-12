0c06e22b4bb6b1d08452c9cef6053ed932b0510b
wchupp <william.chupp@dot.gov>
2023-01-12 15:05:43 -0500

    update repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import logo from './us-department-of-transportation-1.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header">
+      <text className="App-header">
         TRIMS Home Page
-      </h1>
+      </text>
+      <text className="body-text">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>This is where we will describe TRIMS.</p>
+        <p>We will also link to the solution-specific repositories for consumption.</p>
+      </text>
     </div>
   );
 }
