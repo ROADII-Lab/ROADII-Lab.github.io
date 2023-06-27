a209f81e9e2ba04f43cf0d0a098e40c707ad69a5
wchupp <william.chupp@dot.gov>
2023-06-27 19:32:24 -0400

    Update to hashrouter so sub-pages works in gh-pages

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<Router>
 			<App />
-		</BrowserRouter>
+		</Router>
 	</React.StrictMode>
 );
 
